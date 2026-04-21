fe1d761f73674f6f77df019af3888f7cc1ec33d5
KerimovAydin <kerimovaydin177@gmail.com>
2026-04-21 11:24:43 +0000

    axi

---
+++ api/models.py
@@ -0,0 +1,27 @@
+class News:
+
+    def __init__(self, tit, des, img):
+
+        self.tit=tit 
+        self.des=des 
+        self.img=img 
+
+news1=News(
+    "iphone 16 Pro Max",
+    "q4g5t4t",
+    "https://github.com/KerimovAydin/images/blob/main/iphone_16_pro_max.png?raw=true"
+)
+
+news2=News(
+    "AirPods Pro 3",
+    "234t24t",
+    "https://github.com/KerimovAydin/images/blob/main/Apple-AirPods-Pro-3.jpg?raw=true"
+)
+
+news3=News(
+    "Apple Watch Ultra 3",
+    "24t24t",
+    "https://github.com/KerimovAydin/images/blob/main/Apple-Watch-Ultra-3.jpg?raw=true"
+)
+
+news_list=[news1, news2, news3]
